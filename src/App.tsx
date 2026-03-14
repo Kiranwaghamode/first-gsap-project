e75166a8ef45365e2a5ee7bcd502bec946133474
kiranwaghamode <kiranwaghamode99@gmail.com>
2026-03-14 12:58:12 +0530

    first commit of this project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import gsap from 'gsap'
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
+function App() {
+
+  return (
+    <>
+
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl text-blue-500'>Hello Sunita</h1>
+    </div>
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/methods/use-gsap.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const UseGsap = () => {
+
+    useGSAP(()=> {
+        gsap.to('#blue-box', {
+            x: 250,
+            duration: 2,
+            repeat: -1,
+            ease: 'circ.in',
+            rotation: 360,
+            yoyo: true
+        })
+
+
+    }, [])
+
+
+    useGSAP(()=> {
+        gsap.fromTo('.para', {
+            opacity: 0,
+            y: 20
+        },{
+            opacity: 1,
+            y: 0, 
+            delay: 1,
+            stagger: 0.1
+        }
+    )
+
+    }, [])
+    
+
+
+
+
+  return (
+    <div className=''>
+        <div>
+            <h1 className='para'>Hello World</h1>
+            <p className='para'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure quaerat, nostrum doloribus, iste molestiae tempore enim est distinctio architecto magni saepe eos quos eaque libero tempora quisquam optio ipsum reiciendis laborum aliquid earum minus.</p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default UseGsap;
